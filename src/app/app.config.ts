9d1691f1a22970a7b7c793ac8a4edfdfc29083d1
katyaandrushko <katyaandrushko14@gmail.com>
2024-07-23 22:44:13 +0300

    Update angular core

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,7 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {
-  HttpClientModule,
-  provideHttpClient,
-  withFetch,
-  withInterceptors,
-} from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { httpInterceptor } from './core/interceptors/http.interceptor';
 
 export const appConfig: ApplicationConfig = {
