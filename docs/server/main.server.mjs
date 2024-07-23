da36e5f325b0229b9375a30076375718eebb3b63
katyaandrushko <katyaandrushko14@gmail.com>
2024-07-23 22:06:19 +0300

    Add docs

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-4LKVTTHJ.mjs";import"./chunk-MW2LSIH4.mjs";import"./chunk-KKKA6V4G.mjs";import"./chunk-STHALWT4.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
