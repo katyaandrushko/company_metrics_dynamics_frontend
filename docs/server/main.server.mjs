ffdae4a498a8db5e114c86f8452b7209af7138b3
katyaandrushko <katyaandrushko14@gmail.com>
2024-07-24 12:04:57 +0300

    Auto-generated commit

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-4LKVTTHJ.mjs";import"./chunk-MW2LSIH4.mjs";import"./chunk-KKKA6V4G.mjs";import"./chunk-STHALWT4.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
