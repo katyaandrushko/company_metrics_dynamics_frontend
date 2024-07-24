d6a0eb3c65328f0beebfcd899b645e404c4ab257
katyaandrushko <katyaandrushko14@gmail.com>
2024-07-24 12:30:18 +0300

    Remove server.ts

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-LXLWTKCS.mjs";import"./chunk-DVMGX6VX.mjs";import"./chunk-KKKA6V4G.mjs";import"./chunk-B4J55KFK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
